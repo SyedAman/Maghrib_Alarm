9088bb23753cff5f16b94a36133afb4aeb0cc133
Syed Aman <sna274@nyu.edu>
2016-10-09 14:58:34 -0400

    figure out how to run a python script

---
+++ alarm.py
@@ -0,0 +1,10 @@
+from datetime import date
+from datetime import time
+from datetime import datetime
+from datetime import timedelta
+
+def main():
+    var_integer = 76
+    print var_integer
+
+main()
