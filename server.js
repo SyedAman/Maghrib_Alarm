b2a70301f4539efbb1b3d66dc3915ed55e1c8d71
Syed Aman <sna274@nyu.edu>
2017-01-04 20:41:55 -0500

    create GET and POST requests for '/api/bears' route

--- server.js
+++ server.js
@@ -3,16 +3,14 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-
-// use mongoose to interact with database
 const mongoose = require('mongoose');
+
+// connect to MongoDB database
+mongoose.connect('mongodb://syednashikaman:snw0DxALj0P*@ds025459.mlab.com:25459/star-wars-quotes');
 
 // parse html from POST
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-
-// connect to MongoDB database
-mongoose.connect('mongodb://syednashikaman:snw0DxALj0P*@ds025459.mlab.com:25459/star-wars-quotes');
 
 // set server to run on port 8000
 var port = process.env.PORT || 8000;
@@ -20,6 +18,10 @@ var port = process.env.PORT || 8000;
 // define routes for API
 var router = express.Router();
 
+// register and prefix routes
+app.use('/api', router);
+
+// middleware
 router.use((req, res, next) => {
   // log all API requests
   console.log('received request to RESTful API');
@@ -27,16 +29,31 @@ router.use((req, res, next) => {
   next();
 });
 
+// default route
 router.get('/', (req, res) => {
   res.json({
     message: "received a GET request"
   });
 });
 
-router
+// GET and POST requests for '/api/bears'
+router.route('/bears')
 
-// register and prefix routes
-app.use('/api', router);
+  // GET requests
+  .get((req, res) => {
+    res.json({
+      message: "received a GET request for bears"
+    });
+  });
+
+  // POST requests
+  .post((req, res) => {
+    res.json({
+      message: "received a POST request for bears"
+    });
+  });
+
+});
 
 // start and listen to the server
 app.listen(port);
