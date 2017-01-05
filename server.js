4f5cae7bef644f2f5c4f9671a7f393a8f56b5d86
Syed Aman <sna274@nyu.edu>
2017-01-04 19:51:48 -0500

    parse html from POST using body-parser

--- server.js
+++ server.js
@@ -3,3 +3,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+
+// parse html from POST
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
